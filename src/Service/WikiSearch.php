b8e1c0b3d78cd3137f614ef7264544f95aea458c
Matei Stanca <i@ambientimpact.com>
2026-02-04 15:25:28 -0500

    Use service autowiring and add autowiring interface alias.

--- src/Service/WikiSearch.php
+++ src/Service/WikiSearch.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace Drupal\omnipedia_search\Service;
 
-use Drupal\Core\Routing\StackedRouteMatchInterface;
+use Drupal\Core\Routing\RouteMatchInterface;
 use Drupal\omnipedia_search\Service\WikiSearchInterface;
 
 /**
@@ -15,11 +15,11 @@ class WikiSearch implements WikiSearchInterface {
   /**
    * Constructs this service object; saves dependencies.
    *
-   * @param \Drupal\Core\Routing\StackedRouteMatchInterface $currentRouteMatch
+   * @param \Drupal\Core\Routing\RouteMatchInterface $currentRouteMatch
    *   The Drupal current route match service.
    */
   public function __construct(
-    protected readonly StackedRouteMatchInterface $currentRouteMatch,
+    protected readonly RouteMatchInterface $currentRouteMatch,
   ) {}
 
   /**
